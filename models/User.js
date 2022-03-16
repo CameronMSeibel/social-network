c3aa64bc1f5fa88a2d2eaf818ec1f97266264fb0
cmseibel <cam.m.seib@gmail.com>
2022-03-16 14:39:59 -0700

    Write User model.

--- models/User.js
+++ models/User.js
@@ -1,8 +1,29 @@
 const {Schema, model} = require("mongoose");
 
 const userSchema = new Schema({
-
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: /^([a-z\d_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "thought"
+    }],
+    friends: {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+    }
 });
+
+userSchema.virtual("friendCount").get()
 
 const User = model("user", userSchema);
 
