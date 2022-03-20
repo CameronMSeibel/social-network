32faf8afcc3ebd262fb7fd0d2fc4f0c8d7b22f91
cmseibel <cam.m.seib@gmail.com>
2022-03-20 14:27:34 -0700

    Write create routes for Users and Thoughts.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const {Thought} = require("../../models");
-const{validID} = require("../../utils");
+const {Thought, User} = require("../../models");
+const {validID} = require("../../utils");
 
 // Read all thoughts
 router.get("/", async (req, res) => {
@@ -20,12 +20,31 @@ router.get("/:id", async (req, res) => {
 
 // Create new thought
 router.post("/", async (req, res) => {
-
+    const user = await User.findOne({username: req.body.username})
+    if(user){
+        const thought = await Thought.create(req.body);
+        console.log(user);
+        user.thoughts.push(thought._id);
+        user.save();
+        res.json(thought);
+    }else{
+        res.status(404).json({message: `No user found with username ${req.body.username}`});
+    }
 });
 
 // Create new reaction to a thought by thought's ID
 router.post("/:id/reactions", async (req, res) => {
-
+    const id = req.params.id;
+    if(!validID(id)){
+        return res.status(400).json({message: "Provided ID is invalid."})
+    }
+    let thought = await Thought.findById(id);
+    if(!thought){
+        return res.status(404).json({message: `No thought found with ID ${id}`});
+    }
+    thought.reactions.push(req.body);
+    thought = await thought.save();
+    res.json(thought); 
 });
 
 // Update a thought by ID
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
     const id = req.params.id;
     if(!validID(id)){
-        return res.status(400).json({message: "Provided ID is invalid."})
+        return res.status(400).json({message: "Provided ID is invalid."});
     }
     const user = await User.findById(id).select("-__v");
     user ? res.json(user) : res.status(404).json({message: `No user found with ID ${id}`});
@@ -20,12 +20,35 @@ router.get("/:id", async (req, res) => {
 
 // Create new user
 router.post("/", async (req, res) => {
-
+    try{
+        const user = await User.create(req.body);
+        res.json(user);
+    }catch(error){
+        if(error.code === 11000){
+            return res.status(400).json({message: `Sorry, that ${Object.keys(error.keyValue)} is taken.`});
+        }
+        res.status(500).json(error)
+    }
 });
 
 // Create another friend in a users friends list
-router.post("/:id/friends/:_id", async (req, res) => {
-
+router.post("/:id/friends/:friendId", async (req, res) => {
+    const id = req.params.id;
+    const friendId = req.params.friendId
+    if(!validID(id) || !validID(friendId)){
+        return res.status(400).json({message: "Provided ID is invalid."});
+    }
+    const firstUser = await User.findById(id);
+    const secondUser = await User.findById(friendId);
+    if(!firstUser.friends.includes(friendId)){
+        firstUser.friends.push(friendId);
+        secondUser.friends.push(id);
+        firstUser.save();
+        secondUser.save();
+        res.json({message: `User ${id} and ${friendId} are now friends.`});
+    }else{
+        res.status(400).json({message: "Provided users are already friends!"});
+    }
 })
 
 // Update a user by ID
